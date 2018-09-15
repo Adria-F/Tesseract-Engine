53696df33d46e3b68a3dcd424515ba43c1fade6f
Adria-F <adriaferrer2898@gmail.com>
2018-09-15 18:26:31 +0200

    added a close app button

--- ModuleGUI.cpp
+++ ModuleGUI.cpp
@@ -40,6 +40,21 @@ update_status ModuleGUI::Update(float dt)
 {
 	update_status status= UPDATE_CONTINUE;
 
+	ImGui::SetNextWindowPos({ 0,0 });
+	ImGuiWindowFlags window_flags = 0;
+	window_flags |= ImGuiWindowFlags_NoTitleBar;
+	window_flags |= ImGuiWindowFlags_NoMove;
+	window_flags |= ImGuiWindowFlags_NoResize;
+	window_flags |= ImGuiWindowFlags_NoCollapse;
+	ImGui::Begin("Test Menu", 0, window_flags);
+	ImGui::SetWindowSize({ 85, 35 });
+	ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(255, 0, 0, 255));
+	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor(150, 0, 0, 255));
+	ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor(100, 0, 0, 255));
+	if (ImGui::Button("Close App"))
+		status = UPDATE_STOP;
+	ImGui::PopStyleColor(3);
+	ImGui::End();
 	ImGui::ShowDemoWindow();
 
 	return status;
