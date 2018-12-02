66aaf98b0a73e0f8e9f688dc3ce9fc2345d0a287
MaxitoSama <marc.garcia.u@gmail.com>
2018-12-02 23:46:37 +0100

    little timeline update

--- Source/PanelAnimation.cpp
+++ Source/PanelAnimation.cpp
@@ -2,11 +2,13 @@
 #include "ModuleInput.h"
 #include "PanelAnimation.h"
 
-
-
 PanelAnimation::PanelAnimation(const char* name):Panel(name)
 {
 	active = true;
+
+	minSeparation = 50;
+	numFrames = 100;
+	recSize = 700;
 }
 
 
@@ -38,19 +40,20 @@ void PanelAnimation::Draw()
 	ImGui::SameLine();
 
 	//Animation TimeLine
-	ImGui::BeginChild("TimeLine", ImVec2(700, 180), true, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_AlwaysAutoResize);
+	ImGui::BeginChild("TimeLine", ImVec2(700, 180), true, ImGuiWindowFlags_AlwaysAutoResize);
 	ImVec2 p = ImGui::GetCursorScreenPos();
-	for (int i = 0; i < 30; i++)
+
+	for (int i = 0; i < numFrames; i++)
 	{
 		ImGui::BeginGroup();
 				
-		ImGui::GetWindowDrawList()->AddLine({ p.x - mouseMovement.x,p.y }, ImVec2(p.x - mouseMovement.x, p.y + 135), IM_COL32(100, 100, 100, 255), 1.0f);
+		ImGui::GetWindowDrawList()->AddLine({ p.x - mouseMovement.x * 2,p.y }, ImVec2(p.x - mouseMovement.x * 2, p.y + 135), IM_COL32(100, 100, 100, 255), 1.0f);
 
 		char frame[8];
 		sprintf(frame, "%01d", i);
-		ImVec2 aux = { p.x - mouseMovement.x + 3,p.y };
+		ImVec2 aux = { p.x - mouseMovement.x * 2 + 3,p.y };
 		ImGui::GetWindowDrawList()->AddText(aux, ImColor(1.0f, 1.0f, 1.0f, 1.0f), frame);
-		ImGui::GetWindowDrawList()->AddCircleFilled(ImVec2(p.x - mouseMovement.x + 1, p.y + 50), 6.0f, ImColor(0.0f, 0.0f, 1.0f, 1.0f));
+		ImGui::GetWindowDrawList()->AddCircleFilled(ImVec2(p.x - mouseMovement.x*2 + 1, p.y + 50), 6.0f, ImColor(0.0f, 0.0f, 1.0f, 1.0f));
 
 		p = { p.x + 50,p.y };
 
@@ -63,7 +66,7 @@ void PanelAnimation::Draw()
 	ImGui::NewLine();
 
 	ImVec2 leftLimit = { ImGui::GetCursorScreenPos().x, ImGui::GetCursorScreenPos().y + 130 };
-	ImVec2 rightLimit = { ImGui::GetCursorScreenPos().x+100, ImGui::GetCursorScreenPos().y + 145 };
+	ImVec2 rightLimit = { ImGui::GetCursorScreenPos().x+(numFrames>10?recSize/10:recSize/numFrames), ImGui::GetCursorScreenPos().y + 145 };
 	
 	bool mouse = ImGui::IsMouseHoveringRect({ leftLimit.x + mouseMovement.x , leftLimit.y}, {rightLimit.x + mouseMovement.x , rightLimit.y });
 
@@ -78,7 +81,7 @@ void PanelAnimation::Draw()
 
 	if (dragging && ImGui::IsMouseDown(0))
 	{
-		if ( leftLimit.x + mouseMovement.x + ImGui::GetMouseDragDelta(0).x > leftLimit.x && rightLimit.x + mouseMovement.x + ImGui::GetMouseDragDelta(0).x < rightLimit.x + 590)
+		if ( leftLimit.x + mouseMovement.x + ImGui::GetMouseDragDelta(0).x > leftLimit.x && rightLimit.x + mouseMovement.x + ImGui::GetMouseDragDelta(0).x < leftLimit.x+700)
 		{
 			mouseMovement.x += ImGui::GetMouseDragDelta(0).x;
 			mouseMovement.y += ImGui::GetMouseDragDelta(0).y;
--- Source/PanelAnimation.h
+++ Source/PanelAnimation.h
@@ -17,6 +17,11 @@ private:
 
 	ImVec2 mouseMovement = { 0, 0 };
 	bool dragging = false;
+
+	float recSize=700;
+	float minSeparation = 50;
+	float numFrames=1;
+
 };
 
 #endif // !__PANELANIMATION_H__
